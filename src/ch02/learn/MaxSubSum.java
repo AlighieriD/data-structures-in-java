15271a66d30ac309eb059948c8fa3b7060d38cfe
AlighieriDante <greatyuhao8@gmail.com>
2020-05-21 01:03:46 +0800

    ++

--- src/ch02/learn/MaxSubSum.java
+++ src/ch02/learn/MaxSubSum.java
@@ -53,7 +53,67 @@ public class MaxSubSum {
      * @return
      */
     public int maxSubSum3(int[] arr){
-        return 0;
+        return maxSubSum3(arr,0, arr.length-1);
+    }
+
+    // 该方法会返回最大自序和
+    private int maxSubSum3(int[] arr, int l, int r){
+        if (l > r)
+            return 0;
+        if (l == r)
+            return arr[l] > 0 ? arr[l] : 0;
+        int mid = (l + r) / 2;
+        int leftMax = maxSubSum3(arr,l,mid);
+        int rightMax = maxSubSum3(arr,mid+1,r);
+
+        int leftBorderMax = 0, leftBorderSum = 0;
+        for (int i = mid; i >= l; i--) {
+            leftBorderSum += arr[i];
+            leftBorderMax = Math.max(leftBorderMax,leftBorderSum);
+        }
+        int rightBorderMax = 0, rightBorderSum = 0;
+        for (int i = mid+1; i <= r; i++) {
+            rightBorderSum += arr[i];
+            rightBorderMax = Math.max(rightBorderMax,rightBorderSum);
+        }
+        int res = Math.max(leftMax,rightMax);
+        res = Math.max(res,leftBorderMax+rightBorderMax);
+        return res;
+    }
+
+    /**
+     * maxSubSum3 使用O(n) 时间复杂度方法
+     * @param arr
+     * @return
+     */
+    public int maxSubSum4(int[] arr){
+        int l = 0, res = 0, sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            while (sum < 0){
+                sum -= arr[l++];
+            }
+            res = Math.max(res,sum);
+        }
+        return res;
+    }
+
+    /**
+     * maxSubSum3 使用O(n) 时间复杂度方法
+     * @param arr
+     * @return
+     */
+    public int maxSubSum4_2(int[] arr){
+        int res = 0, sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (sum > res){
+                res = sum;
+            }else if (sum < 0){
+                sum = 0;
+            }
+        }
+        return res;
     }
 
 }
--- test/ch02/learn/MaxSubSumTest.java
+++ test/ch02/learn/MaxSubSumTest.java
@@ -36,4 +36,42 @@ class MaxSubSumTest {
         Assertions.assertEquals(20,maxSubSum.maxSubSum2(arr));
     }
 
+    @Test
+    void maxSubSum3() {
+        int[] arr = {-2,11,-4,13,-5,-2,-10};
+        Assertions.assertEquals(20,maxSubSum.maxSubSum3(arr));
+    }
+
+    @Test
+    void maxSubSum3_1() {
+        int[] arr = {4,-3,5,-2,-1,2,6,-2};
+        Assertions.assertEquals(11,maxSubSum.maxSubSum3(arr));
+    }
+
+    @Test
+    void maxSubSum4() {
+        int[] arr = {4,-3,5,-2,-1,2,6,-2};
+        Assertions.assertEquals(11,maxSubSum.maxSubSum4(arr));
+    }
+
+
+    @Test
+    void maxSubSum4_1() {
+        int[] arr = {-2,11,-4,13,-5,-2,-10};
+        Assertions.assertEquals(20,maxSubSum.maxSubSum4(arr));
+    }
+
+    @Test
+    void maxSubSum4_2() {
+        int[] arr = {4,-3,5,-2,-1,2,6,-2};
+        Assertions.assertEquals(11,maxSubSum.maxSubSum4_2(arr));
+    }
+
+
+    @Test
+    void maxSubSum4_2_1() {
+        int[] arr = {-2,11,-4,13,-5,-2,-10};
+        Assertions.assertEquals(20,maxSubSum.maxSubSum4_2(arr));
+    }
+
 }
