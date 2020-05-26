e9e78e0281e8e8439b99a0004b6c5a75d08a29a0
AlighieriDante <greatyuhao8@gmail.com>
2020-05-27 02:29:31 +0800

    a

--- src/ch03/exercise/pr02/Pr02.java
+++ src/ch03/exercise/pr02/Pr02.java
@@ -25,44 +25,48 @@ public class Pr02 {
             size++;
         }
 
-
         /**
-         * 通过只调整链（而不是数据）来交换两个相邻的元素。
-         * 通用接口
-         * 是否相邻，是否是头尾节点，都需要考虑
+         * https://www.geeksforgeeks.org/swap-nodes-in-a-linked-list-without-swapping-data/
          * @param i
          * @param j
          */
-        public void swap(int i, int j){
-            // TODO i j 是否符合规则
-            Node<E> f = first;
-            Node<E> prei = null, prej = null;
+        public void swap(int i , int j){
+            // 检查 i j 下标是否合法
+            check(i);
+            check(j);
+            // 保证 j > i
+            if (i > j){
+                swap(j,i);
+                return;
+            }
+            // 找到prei 和 prej
+            Node<E> head = new Node<>(null,first);
             int start = 0;
-            // TODO 此处假设j > i
-            while (f != null && start <= j){
-                if (start == i - 1){
-                    prei = f;
-                } else if (start == j - 1){
-                    prej = f;
+            Node<E> prei = null, prej = null;
+            while (head != null && start <= j){
+                if (start == i){
+                    prei = head;
+                }
+                if (start == j){
+                    prej = head;
                 }
                 start++;
-                f = f.next;
+                head = head.next;
             }
-            // i == 0的情况
-            if (prei == null){
 
-            }else {
-                prei.next.next = prej.next.next;
-                prei.next = prej.next;
-                if (j - i == 1){
-                    prei.next.next = prej;
-                }else {
-                    prei.next.next
-                }
+        }
+
+        private void check(int i){
+            if (i < 0 || i > size){
+                throw new IndexOutOfBoundsException();
             }
         }
+
     }
     public class DoubleLinkedList<E>{
 
+    }
+
+    public static void main(String[] args) {
     }
 }
