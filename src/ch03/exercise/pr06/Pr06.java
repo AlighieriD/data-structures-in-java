b9360fb53188ea65d391f279b72c49a56d2779b7
asiuf <greatyuhao8@gmail.com>
2020-06-01 18:24:44 +0800

    w

--- src/ch03/exercise/pr06/Pr06.java
+++ src/ch03/exercise/pr06/Pr06.java
@@ -1,7 +1,15 @@
 package ch03.exercise.pr06;
 
+import java.util.LinkedList;
+
 /**
  * Created by YH on 2020/6/1.
  */
 public class Pr06 {
+
+    public int josephus(int n, int m){
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
 }
