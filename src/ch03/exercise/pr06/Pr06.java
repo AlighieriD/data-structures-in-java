6be61b62769d0b2b1aa291d823f8d74689e6f529
AlighieriDante <greatyuhao8@gmail.com>
2020-06-03 02:48:33 +0800

    h

--- src/ch03/exercise/pr06/Pr06.java
+++ src/ch03/exercise/pr06/Pr06.java
@@ -7,9 +7,24 @@ import java.util.LinkedList;
  */
 public class Pr06 {
 
-    public int josephus(int n, int m){
-        for (int i = 0; i < n; i++) {
-
+    private static class Node{
+        private int i;
+        private Node pre;
+        private Node next;
+        Node(Node pre, int i, Node next){
+            this.i = i;
+            this.pre = pre;
+            this.next = next;
         }
+    }
+    public int josephus(int n, int m){
+        Node head = new Node(null,-1,null);
+        Node curr = head;
+        for (int i = 0; i < n; i++) {
+            curr.next = new Node(curr,i+1,null);
+            curr = curr.next;
+        }
+        curr.next = head.next;
+        
     }
 }
