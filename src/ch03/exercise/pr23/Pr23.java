9b82b7a971875858d518d48d0e4ea676e10ca30e
AlighieriDante <greatyuhao8@gmail.com>
2020-06-16 01:53:40 +0800

    h

--- src/ch03/exercise/pr23/Pr23.java
+++ src/ch03/exercise/pr23/Pr23.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 public class Pr23 {
 
-    //优先级表
     public static Map<String,Integer> map = new HashMap(){
         {
             put("+",1);
@@ -13,36 +12,45 @@ public class Pr23 {
             put("/",2);
             put("^",3);
             put("(",4);
-            put(")",4);
+            put(")",1);
         }
     };
+
     public static String[] inToPostFix(String[] inFix) {
         String[] postFix = new String[inFix.length];
         int idx = 0;
         Deque<String> stack = new LinkedList<>();
         for (int i = 0; i < inFix.length; i++) {
             String s = inFix[i];
-            if ("+".equals(s) ||"-".equals(s) ||"*".equals(s) ||"/".equals(s) ||"^".equals(s) ||"(".equals(s) ||")".equals(s)){
-                while (stack.size() != 0 && map.get(s) <= map.get(stack.peek())){
-                    if ("(".equals(stack.peek()) && !")".equals(s))
+            if ("+".equals(s) || "-".equals(s) || "*".equals(s) || "/".equals(s) || "^".equals(s) || "(".equals(s)){
+                while (stack.size() > 0 && map.get(s) <= map.get(stack.peek())){
+                    if ("(".equals(stack.peek()))
                         break;
-                    String pop = stack.pop();
-                    if (!"(".equals(pop))
-                        postFix[idx++] = pop;
+                    postFix[idx++] = stack.pop();
                 }
                 if (!")".equals(s))
                     stack.push(s);
+            }else if (")".equals(s)){
+                while (stack.size() > 0 && !"(".equals(stack.peek())){
+                    postFix[idx++] = stack.pop();
+                }
+                stack.pop();
             }else {
                 postFix[idx++] = s;
             }
         }
-        while (stack.size() != 0){
+        while (stack.size() != 0)
             postFix[idx++] = stack.pop();
-        }
         return postFix;
     }
 
+    public static String[] postToInFix(String[] postFix){
+        String[] inFix = new String[postFix.length];
+        return null;
+    }
+
     public static void main(String[] args) {
+//        String[] arr = {"a","^","b","^","2"};
         String[] arr = {"a","+","b","*","c","+","(","d","*","e","+","f",")","*","g"};
         String[] res = inToPostFix(arr);
         System.out.println(Arrays.toString(res));
---
+++ src/ch03/exercise/pr24/Pr24.java
@@ -0,0 +1,21 @@
+package ch03.exercise.pr24;
+
+/**
+ * Created by YH on 2020/6/16.
+ */
+public class Pr24 {
+    public static class TwoStack<E>{
+        E[] items;
+        int head1;
+        int head2;
+        TwoStack(int size){
+            items = (E[]) new Object[size];
+            head1 = 0;
+            head2 = size - 1;
+        }
+
+        void push1(E e){
+            items[]
+        }
+    }
+}
