31eb8bc0ee2fdf8e4d8b2b9e41e2c02cda8cffd9
asiuf <greatyuhao8@gmail.com>
2020-07-09 18:43:05 +0800

    w

--- src/ch03/exercise/pr34/Pr34.java
+++ src/ch03/exercise/pr34/Pr34.java
@@ -7,6 +7,53 @@ import java.util.*;
  */
 public class Pr34 {
 
+    protected static class Node<E> {
+        public E e;
+        public Node<E> next;
+        Node(E e){
+            this.e = e;
+        }
+    }
+
+    protected static class MyCicleList<E> implements Iterable<E>{
+        protected Node<E> head = new Node<>(null);
+        protected Node<E> last = null;
+        protected int size;
+
+        public void append(Node<E> node){
+            if (last == null){
+                head.next = node;
+                last = node;
+            } else {
+                last.next = node;
+                last = node;
+            }
+            size++;
+        }
+
+        @Override
+        public Iterator<E> iterator() {
+            return new MyItr();
+        }
+
+        private class MyItr implements Iterator<E>{
+            Node<E> first = head.next;
+            @Override
+            public boolean hasNext() {
+                return first != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext())
+                    throw new NoSuchElementException();
+                E e = first.e;
+                first = first.next;
+                return e;
+            }
+        }
+    }
+
     /**
      * 如果从某个节点p开始，接着跟有足够数目的next链将把我们带回到节点p，
      * 那么这个链表包含一个循环。p不必是该表的第一个节点。假设给你一个链表，
@@ -16,11 +63,12 @@ public class Pr34 {
     /**
      * a. 设计一个O(N)算法以确定该表是否包含有循环。你可以使用O(N)的额外空间。
      */
-    public static <E> boolean isCicle(List<E> list){
+    public static <E> boolean isCicle(MyCicleList<E> list){
         Map<E,Boolean> map = new HashMap<>();
         for (E e : list){
             if (map.get(e) != null && map.get(e))
                 return true;
+            map.put(e,true);
         }
         return false;
     }
@@ -29,7 +77,7 @@ public class Pr34 {
      * b. 重复(a)部分，但是只使用O(1)的额外空间。
      *（提示，使用两个迭代器，它们最初在表的开始处，但以不停的速度推进）。
      */
-    public static <E> boolean isCicle2(List<E> list){
+    public static <E> boolean isCicle2(MyCicleList<E> list){
         Iterator<E> itr1 = list.iterator();
         Iterator<E> itr2 = list.iterator();
         do {
@@ -39,4 +87,5 @@ public class Pr34 {
         }while (itr1.hasNext());
         return false;
     }
+
 }
--- test/ch03/exercise/pr34/Pr34Test.java
+++ test/ch03/exercise/pr34/Pr34Test.java
@@ -1,5 +1,6 @@
 package ch03.exercise.pr34;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
@@ -11,15 +12,43 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class Pr34Test {
     @Test
-    void isCicle() {
-        LinkedList<String> l = new LinkedList<>();
-        l.add("A");
-        l.add("B");
-
+    void isCicleTest() {
+        Pr34.MyCicleList<String> m = new Pr34.MyCicleList<>();
+        Pr34.Node<String> n1 = new Pr34.Node<>("A");
+        Pr34.Node<String> n2 = new Pr34.Node<>("B");
+        Pr34.Node<String> n3 = new Pr34.Node<>("C");
+        Pr34.Node<String> n4 = new Pr34.Node<>("D");
+        Pr34.Node<String> n5 = new Pr34.Node<>("E");
+        Pr34.Node<String> n6 = new Pr34.Node<>("F");
+        Pr34.Node<String> n7 = n6;
+        m.append(n1);
+        m.append(n2);
+        m.append(n3);
+        m.append(n4);
+        m.append(n5);
+        m.append(n6);
+        m.append(n7);
+        Assertions.assertTrue(Pr34.isCicle(m));
     }
 
     @Test
     void isCicle2() {
+        Pr34.MyCicleList<String> m = new Pr34.MyCicleList<>();
+        Pr34.Node<String> n1 = new Pr34.Node<>("V");
+        Pr34.Node<String> n2 = new Pr34.Node<>("B");
+        Pr34.Node<String> n3 = new Pr34.Node<>("C");
+        Pr34.Node<String> n4 = new Pr34.Node<>("D");
+        Pr34.Node<String> n5 = new Pr34.Node<>("E");
+        Pr34.Node<String> n6 = new Pr34.Node<>("F");
+        Pr34.Node<String> n7 = n3;
+        m.append(n1);
+        m.append(n2);
+        m.append(n3);
+        m.append(n4);
+        m.append(n5);
+        m.append(n6);
+        m.append(n7);
+        Assertions.assertTrue(Pr34.isCicle(m));
     }
 
 }
