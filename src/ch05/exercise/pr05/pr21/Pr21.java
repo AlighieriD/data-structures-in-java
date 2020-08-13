f4cc991f2f89c36333805efa594c3cbcf3af63c3
AlighieriDante <greatyuhao8@gmail.com>
2020-08-13 08:08:55 +0800

    a

---
+++ src/ch05/exercise/pr05/pr21/Pr21.java
@@ -0,0 +1,7 @@
+package ch05.exercise.pr05.pr21;
+
+/**
+ * Created by YH on 2020/8/12.
+ */
+public class Pr21 {
+}
---
+++ src/ch05/exercise/pr05/pr24/Pr24.java
@@ -0,0 +1,7 @@
+package ch05.exercise.pr05.pr24;
+
+/**
+ * Created by YH on 2020/8/12.
+ */
+public class Pr24 {
+}
